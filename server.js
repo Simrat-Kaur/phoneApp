31f54cef895b97cd37c790f1e04ffc77c46b043c
Simrat <sktakhar333@gmail.com>
2020-05-28 12:57:06 +0530

    changed port

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+var PORT=process.env.PORT|| 3000;
 require('./models/db');
+
 
 const express = require('express');
 const path = require('path');
@@ -16,7 +18,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Express server started at port : 3000');
 });
 
